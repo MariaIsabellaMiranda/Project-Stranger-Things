37e0955dbb5288a7cbbce2a912eec9f24afedbab
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-12 01:23:11 -0300

    feat: Criada nova variavel para controlar frase na aplicação.

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -113,6 +113,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
+        {REACT_APP_DEVELOPMENT === 'true' && <h1>Em desenvolvimento</h1>}
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
